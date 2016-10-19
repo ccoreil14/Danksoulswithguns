8c3d8be8fc26440ba59e5b1b3f7df54187040246
Alexander Devine <alexdevine387@gmail.com>
2016-10-19 08:56:12 -0600

    Added psuedo code to createplayer, as well as movement logic in keyboardClicks.js, also added logic for checking inbounds

--- assets/scripts/keyboardClicks.js
+++ assets/scripts/keyboardClicks.js
@@ -7,36 +7,89 @@ var KEYCODE_A = 65;
 var KEYCODE_W = 87;
 var KEYCODE_D = 68;
 var KEYCODE_S = 83;
- 
+
 function handleKeyDown(evt) {
-    if(!evt){ var evt = window.event; }
-    switch(evt.keyCode) {
-        case KEYCODE_LEFT:  console.log(evt.keyCode+" down"); return false;
-        case KEYCODE_RIGHT: console.log(evt.keyCode+" down"); return false;
-        case KEYCODE_UP:    console.log(evt.keyCode+" down"); return false;
-        case KEYCODE_DOWN:  console.log(evt.keyCode+" down"); return false;
-		case KEYCODE_SPACE:  console.log(evt.keyCode+" down"); return false;
-		case KEYCODE_A:  console.log(evt.keyCode+" down"); return false;
-		case KEYCODE_W:  console.log(evt.keyCode+" down"); return false;
-		case KEYCODE_D:  console.log(evt.keyCode+" down"); return false;
-		case KEYCODE_S:  console.log(evt.keyCode+" down"); return false;
+    if (!evt) {
+        var evt = window.event;
+    }
+    switch (evt.keyCode) {
+    case KEYCODE_LEFT:
+
+        return false;
+    case KEYCODE_RIGHT:
+
+        return false;
+    case KEYCODE_UP:
+
+        return false;
+    case KEYCODE_DOWN:
+
+        return false;
+    case KEYCODE_SPACE:
+
+        return false;
+        //Below the + or - 30 represents where we want the players bounds to be
+    case KEYCODE_A:
+        if (inbounds(player.x - 30, player.y)) {
+            player.x -= 10;
+        }
+        return false;
+    case KEYCODE_W:
+        if (inbounds(player.x, player.y - 30)) {
+            player.y -= 10;
+        }
+        return false;
+    case KEYCODE_D:
+        if (inbounds(player.x + 30, player.y)) {
+            player.x += 10;
+        }
+        return false;
+    case KEYCODE_S:
+        if (inbounds(player.x, player.y + 30)) {
+            player.y += 10
+        }
+        return false;
     }
 }
- 
+
 function handleKeyUp(evt) {
-    if(!evt){ var evt = window.event; }
-    switch(evt.keyCode) {
-        case KEYCODE_LEFT:    console.log(evt.keyCode+" up"); break;
-        case KEYCODE_RIGHT:   console.log(evt.keyCode+" up"); break;
-        case KEYCODE_UP:      console.log(evt.keyCode+" up"); break;
-        case KEYCODE_DOWN:    console.log(evt.keyCode+" up"); break;
-		case KEYCODE_SPACE:  console.log(evt.keyCode+" up"); return false;
-		case KEYCODE_A:  console.log(evt.keyCode+" up"); return false;
-		case KEYCODE_W:  console.log(evt.keyCode+" up"); return false;
-		case KEYCODE_D:  console.log(evt.keyCode+" up"); return false;
-		case KEYCODE_S:  console.log(evt.keyCode+" up"); return false;
+    if (!evt) {
+        var evt = window.event;
+    }
+    switch (evt.keyCode) {
+    case KEYCODE_LEFT:
+
+        break;
+    case KEYCODE_RIGHT:
+
+        break;
+    case KEYCODE_UP:
+
+        break;
+    case KEYCODE_DOWN:
+
+        break;
+    case KEYCODE_SPACE:
+
+        return false;
+    case KEYCODE_A:
+
+        return false;
+    case KEYCODE_W:
+
+        return false;
+    case KEYCODE_D:
+
+        return false;
+    case KEYCODE_S:
+
+        return false;
     }
 }
- 
+
+stage.on("stagemousedown", function (evt) {
+    //Add shoot function here, we may need to add a variable for a shot delay+
+})
+
 document.onkeydown = handleKeyDown;
 document.onkeyup = handleKeyUp;
