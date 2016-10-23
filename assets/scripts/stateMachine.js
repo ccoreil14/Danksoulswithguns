b0194c8379fc39c317a1d10b8f2ef1975099b6b3
ccoreil14 <ccoreil14@gmail.com>
2016-10-23 12:26:29 -0700

    Player is gif and moves as well as can be moved and only appears on game start

--- assets/scripts/stateMachine.js
+++ assets/scripts/stateMachine.js
@@ -1,55 +1,59 @@
 var state = 000;
 
-function stateChange(){
-	switch (state){
-		//loading state
-		case 000:
-			break;
-		//title state
-		case 100:
-			finalScore.text = "";
-			instructionsButton.visible = true;
-			playButton.visible = true;
-			menuButton.visible = false;
-			titleScreen.visible = true;
-			instructionsScreen.visible = false;
-			playScreen.visible = false;
-			overScreen.visible = false;
-			break;
-		//instructions state
-		case 200:
-			instructionsButton.visible = false;
-			playButton.visible = true;
-			menuButton.visible = true;
-			titleScreen.visible = false;
-			instructionsScreen.visible = true;
-			playScreen.visible = false;
-			overScreen.visible = false;
-			break;
-		//game state
-		case 300:
-			looop = true;
-			instructionsButton.visible = false;
-			playButton.visible = false;
-			menuButton.visible = false;
-			titleScreen.visible = false;
-			instructionsScreen.visible = false;
-			playScreen.visible = true;
-			overScreen.visible = false;
-			break;
-		//game over state
-		case 400:
-			walk.visible = false;
-			looop = false;
-			addFinalScore();
-			score.text = "Score: 0";
-			instructionsButton.visible = false;
-			playButton.visible = false;
-			menuButton.visible = true;
-			titleScreen.visible = false;
-			instructionsScreen.visible = false;
-			playScreen.visible = false;
-			overScreen.visible = true;
-			break;
-	}
+function stateChange() {
+    switch (state) {
+        //loading state
+    case 000:
+        break;
+        //title state
+    case 100:
+        finalScore.text = "";
+        instructionsButton.visible = true;
+        playButton.visible = true;
+        menuButton.visible = false;
+        titleScreen.visible = true;
+        instructionsScreen.visible = false;
+        playScreen.visible = false;
+        overScreen.visible = false;
+        player.visible = false;
+        break;
+        //instructions state
+    case 200:
+        instructionsButton.visible = false;
+        playButton.visible = true;
+        menuButton.visible = true;
+        titleScreen.visible = false;
+        instructionsScreen.visible = true;
+        playScreen.visible = false;
+        overScreen.visible = false;
+        break;
+        //game state
+    case 300:
+        player.x = 400;
+        player.y = 380;
+        looop = true;
+        instructionsButton.visible = false;
+        playButton.visible = false;
+        menuButton.visible = false;
+        titleScreen.visible = false;
+        instructionsScreen.visible = false;
+        playScreen.visible = true;
+        overScreen.visible = false;
+        player.visible = true;
+        break;
+        //game over state
+    case 400:
+        player.visible = false;
+        looop = false;
+        addFinalScore();
+        score.text = "Score: 0";
+        instructionsButton.visible = false;
+        playButton.visible = false;
+        menuButton.visible = true;
+        titleScreen.visible = false;
+        instructionsScreen.visible = false;
+        playScreen.visible = false;
+        overScreen.visible = true;
+        break;
+    }
 }
--- setup.js
+++ setup.js
@@ -20,10 +20,10 @@ manifest = [
 	{src:"images/instructions.jpg", id:"instructions"},
 	{src:"images/title.jpg", id:"title"},
 	{src:"images/play.jpg", id:"playarea"},
-	{src:"images/sprites.png", id:"sprites"},
 	{src:"images/instructionsButton.png", id:"instructionsButton"},
 	{src:"images/playButton.png", id:"playButton"},
 	{src:"images/menuButton.png", id:"menuButton"},
+	{src:"images/ufo.png", id:"ufo"},
 	{src:"scripts/keyboardClicks" + jsEnd},
 	{src:"scripts/buildGameOver" + jsEnd},
 	{src:"scripts/buildPlayArea" + jsEnd},
@@ -32,6 +32,7 @@ manifest = [
 	{src:"scripts/buttonControls" + jsEnd},
 	{src:"scripts/mainLoop" + jsEnd},
 	{src:"scripts/mouseInfo" + jsEnd},
+	{src:"scripts/createPlayer" + jsEnd},
 	{src:"scripts/stateMachine" + jsEnd}
 	
 ];
@@ -44,29 +45,26 @@ function loadFiles() {
     queue.loadManifest(manifest);
 }
 
-var walk, blocks, blockArray;
+var blocks, blockArray;
 
 blockArray = [];
 
 function displaySprites() {
-    walk.x=400;
-    walk.y=380;
-    walk.gotoAndPlay("walkRight");
-    stage.addChild(walk);
 	
-	for(i = 0; i < 5; i++){
-		blocks.x=i*31+20;
-		blocks.y=215;
-		blocks.gotoAndStop(i);
-		blockArray.push(blocks.clone());
-	}
-	for(j = 0; j < 5; j++){
-		stage.addChild(blockArray[j]);	
-	}
+//	for(i = 0; i < 5; i++){
+//		blocks.x=i*31+20;
+//		blocks.y=215;
+//		blocks.gotoAndStop(i);
+//		blockArray.push(blocks.clone());
+//	}
+//	for(j = 0; j < 5; j++){
+//		stage.addChild(blockArray[j]);	
+//	}
 }
  
 function loadComplete(evt) {
-	createPlayArea();
+    createPlayArea();
+	createPlayer();
 	createGameTimer();
 	createGameOver();
 	createTitle();
@@ -74,6 +72,7 @@ function loadComplete(evt) {
 	setupButtons();
 	initCoorText();
 	mouseInit();
+    
 }
 
 (function main(){
