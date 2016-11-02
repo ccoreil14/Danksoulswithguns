86b15b84fd7eb07f8726b6796dc8ab6ecb688f6f
Alexander Devine <alexdevine387@gmail.com>
2016-11-01 19:28:08 -0600

    Fixed iframes for when you get hit by blue and green turrets

--- assets/scripts/lazerBullet.js
+++ assets/scripts/lazerBullet.js
@@ -5,32 +5,32 @@ var lazerBulletSpeed = 15;
 function makeLazerBullet() {
     for (var i = 0; i < gTurrets.length; i++) {
         lazerBullet = new createjs.Bitmap(queue.getResult("lazerBullet"));
-		
-		lazerBullet.scaleX = lazerBullet.scaleY = .7;
+
+        lazerBullet.scaleX = lazerBullet.scaleY = .7;
         if (gTurrets[i].rotation === 0) {
             lazerBullet.x = gTurrets[i].x + 10;
             lazerBullet.y = gTurrets[i].y - 25;
-			
+
             lazerBullet.rotation = 0;
-			
+
         } else if (gTurrets[i].rotation === 90) {
             lazerBullet.x = gTurrets[i].x + 25;
             lazerBullet.y = gTurrets[i].y;
-			
+
             lazerBullet.rotation = 90;
-			
+
         } else if (gTurrets[i].rotation === 180) {
             lazerBullet.x = gTurrets[i].x - 10;
             lazerBullet.y = gTurrets[i].y + 25;
-			
+
             lazerBullet.rotation = 180;
-			
+
         } else if (gTurrets[i].rotation === 270) {
             lazerBullet.x = gTurrets[i].x - 25;
             lazerBullet.y = gTurrets[i].y - 10;
-			
+
             lazerBullet.rotation = 270;
-			
+
         }
 
         lazerBullets.push(lazerBullet);
@@ -64,6 +64,7 @@ function updateLazerBullets() {
 function checkLazerBulletCollision() {
     for (var i = 0; i < lazerBullets.length; i++) {
         if (ndgmr.checkRectCollision(lazerBullets[i], player) != null && player.tooStrong === false) {
+            player.gotHit = true;
             healthSize -= .1;
             stage.removeChild(lazerBullets[i]);
             lazerBullets.splice(i--, 1);
